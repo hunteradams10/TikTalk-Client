2ec379fb29023eee8064adca7bbd9aea6c505c1b
Hunter Adams <90118549+hunteradams10@users.noreply.github.com>
2022-11-13 19:13:07 +1100

    deleted context, don't need it for now

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,11 +1,8 @@
 import SideBar from "./SideBar.jsx";
 import Message from "./Message.jsx";
-import LoginBar from "./LoginBar.jsx";
-
 function Chat() {
   return (
     <div className="chat">
-      <LoginBar/>
       <SideBar /> 
     <Message />
     </div>

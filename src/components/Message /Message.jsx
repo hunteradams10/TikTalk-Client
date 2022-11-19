bd505a27ef382d3409ca86dc4e79fa86d73bca22
Hunter Adams <90118549+hunteradams10@users.noreply.github.com>
2022-11-19 13:59:32 +1100

    Revert "position message"

--- src/components/Message /Message.jsx
+++ src/components/Message /Message.jsx
@@ -2,12 +2,10 @@ import './message.css'
 
 import React from 'react'
 
-function Message({data,userdata}) {
-
-  //console.log(data.senderId)
-  //console.log(userdata.userId)
+function Message({data}) {
   return (
-    <div className= {data.senderId === userdata.userId ? "message own" : "message"}>
+    <div className="message">
+      {/* {own ? "message own" : "message"}> */}
         <div className="message-top">
             <img className="message-image" src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?cs=srgb&dl=pexels-stefan-stefancik-91227.jpg&fm=jpg&_gl=1*nmfcye*_ga*NzgwNjU2MzM1LjE2NjgxNTI0MzY.*_ga_8JE65Q40S6*MTY2ODMzMDAzMy41LjEuMTY2ODMzMjIzMy4wLjAuMA.." alt="" />
             <p className="message-text">{data.message}</p>

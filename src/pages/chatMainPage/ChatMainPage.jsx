bdbe042c1650f15a68672a014200c940e0b655fa
coderGirlSu <95465740+coderGirlSu@users.noreply.github.com>
2022-11-19 12:02:03 +1100

    get jwt from local storage

--- src/pages/chatMainPage/ChatMainPage.jsx
+++ src/pages/chatMainPage/ChatMainPage.jsx
@@ -5,10 +5,13 @@ import UserOnline from '../../components/userOnline/UserOnline'
 import './chatMainPage.css'
 import Axios from "axios"
 import Navbar from '../../components/Navbar/Navbar'
+import { useNavigate } from "react-router-dom";
 
 function ChatMainPage() {
-  let conversationId = "6373403e0146ddf60826ebe7"
-  let jwt = "eyJhbGciOiJSUzI1NiIsImtpZCI6ImY4MDljZmYxMTZlNWJhNzQwNzQ1YmZlZGE1OGUxNmU4MmYzZmQ4MDUiLCJ0eXAiOiJKV1QifQ.eyJuYW1lIjoiZGFkYWRhIiwicmVndWxhclVzZXIiOnRydWUsImlzcyI6Imh0dHBzOi8vc2VjdXJldG9rZW4uZ29vZ2xlLmNvbS90aWstdGFsay1kYzAxOCIsImF1ZCI6InRpay10YWxrLWRjMDE4IiwiYXV0aF90aW1lIjoxNjY4NzcyOTQ3LCJ1c2VyX2lkIjoicVplOUEwQzBLQ1BudEwwZVoyOUtUS2thb0tIMiIsInN1YiI6InFaZTlBMEMwS0NQbnRMMGVaMjlLVEtrYW9LSDIiLCJpYXQiOjE2Njg3NzI5NDcsImV4cCI6MTY2ODc3NjU0NywiZW1haWwiOiJ0ZXN0MTNAdGVzdDEzLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7ImVtYWlsIjpbInRlc3QxM0B0ZXN0MTMuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoicGFzc3dvcmQifX0.rW9Qmo82RQLY4d6279teCadkdmbbGCdwi1SG7uoDN9eGDKNwmgZQPVzZ0Ze_V7o7lmDpXotJDWAolxIJVndtjwZbPOqlOv4liL_jOFgKeRmf34vqaQCFuFDHqmDwDsYJ--CJT_nbvbUcYuvF6IzXXgPs2YOvLbUuBiULCM2p9OpdrlGMGhZouFIDYMcmJxZskgyyv-NBCLXO93kNGJTmKwUB0BqKOxFf8LMwV5if_7MuQJL4e_U0F7fUWcBHOfAwj9bdz_zM_VmUXV8Nat5CRVYzYROApNsvQuF8p-0mlx9C9ZZL4FhQAOiqB-RN2mXe8BF8BMnB6Gpgm7LTeTr88Q"
+  let nav = useNavigate()
+  let conversationId = "6373403e0146ddf60826ebe7" 
+
+
  
 
   const [history, setHistory] = useState([])
@@ -16,10 +19,18 @@ function ChatMainPage() {
 
 async function fetchData(){
     try{
+      let userData = localStorage.getItem("user")
+      if (userData == null) {
+        nav("/")
+      }
+
+      let userObj = JSON.parse(userData)  // get user data and convert user string to object
+      let jwt = userObj.jwt
+
         const url = "https://tiktalk-server.codergirlsu.dev/groups/history?groupId=" + conversationId
         const res = await Axios.get(url, {headers: { 'Authorization': "Bearer " + jwt }})
         console.log(">>>>" + JSON.stringify(res.data))
-      setHistory(res.data)
+        setHistory(res.data)
 
       } catch(error) {
         //setError(error.response.data.message)
